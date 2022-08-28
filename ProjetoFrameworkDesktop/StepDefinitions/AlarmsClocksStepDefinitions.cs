df718c99e9429d548bb6b040f7de57edf408f103
David Brandao <davfb.ferreira@gmail.com>
2022-08-28 18:08:43 -0300

    Ajustando arquivo de steps

--- ProjetoFrameworkDesktop/StepDefinitions/AlarmsClocksStepDefinitions.cs
+++ ProjetoFrameworkDesktop/StepDefinitions/AlarmsClocksStepDefinitions.cs
@@ -12,15 +12,10 @@ namespace FrameworkDesktop
     public class AlarmsClocksStepDefinitions : Hooks
     {
 
-        WindowsDriver<WindowsElement> Driver;
-
         [Given(@"Alarms&Clocks app is opened")]
         public void GivenAlarmsClocksAppIsOpened()
         {
-            /*AppiumOptions capabilities = new AppiumOptions();
-            capabilities.AddAdditionalCapability("app", "Microsoft.WindowsAlarms_8wekyb3d8bbwe!App");
-            Driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), capabilities);
-            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);*/
+            // Verificar se a aplicação se encontra aberta
             alarmsClocksSession.FindElementByAccessibilityId("MenuItemsScrollViewer");
         }
 
@@ -30,11 +25,10 @@ namespace FrameworkDesktop
             // Abrir página do alarme
             alarmsClocksSession.FindElementByAccessibilityId("AlarmButton").Click();
 
-
-            //Clicar no botão adicionar alarme
+            // Clicar no botão adicionar alarme
             alarmsClocksSession.FindElementByAccessibilityId("AddAlarmButton").Click();
 
-            //Clicar no botão salvar
+            // Clicar no botão salvar
             alarmsClocksSession.FindElementByAccessibilityId("PrimaryButton").Click();
         }
 
@@ -43,9 +37,6 @@ namespace FrameworkDesktop
         {
             // VErificar se um alarme é retornado na lista de items
             Assert.IsTrue(alarmsClocksSession.FindElementsByAccessibilityId("AlarmViewGrid").Count > 0);
-
-            //Fechar a sessão da aplicação
-            //Driver.Quit();
         }
     }
 }
